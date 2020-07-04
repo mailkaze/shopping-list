3dcac35d81c4d7cd0e1989523ada13c633785a24
unknown <mailkaze@gmail.com>
2020-07-04 00:14:16 -0400

    start

---
+++ src/components/ShowControl.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ShowControl = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app'
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCuvbUG9b0QKlb_f9cUavhT2iUi2XmHr0Q",
+    authDomain: "shopping-list-1d506.firebaseapp.com",
+    databaseURL: "https://shopping-list-1d506.firebaseio.com",
+    projectId: "shopping-list-1d506",
+    storageBucket: "shopping-list-1d506.appspot.com",
+    messagingSenderId: "219622571466",
+    appId: "1:219622571466:web:00e79a532fe70858f96333"
+  };
+  // Initialize Firebase
+  const fb = firebase.initializeApp(firebaseConfig);
+  export const db = fb.firestore()

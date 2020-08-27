c2ecfbcd96457e0d46a80acb08800005da84f060
unknown <mailkaze@gmail.com>
2020-08-27 15:59:26 -0400

    parche para elementosd borrados

--- src/components/List.js
+++ src/components/List.js
@@ -7,6 +7,7 @@ export const List = ({ marked, orderedElements }) => {
   const search = useSelector(state => state.search)
   
   const filterSearch = () => {
+    orderedElements.map((o, index) => {if (o === undefined) {console.log('undefined en la lista ' + marked + ' con index ' + index)}})
     if (search) {
       return orderedElements.filter(e => 
         e.name.toLowerCase().includes(search.toLowerCase().trim()))
@@ -22,7 +23,7 @@ export const List = ({ marked, orderedElements }) => {
           <div className="list-container" ref={provided.innerRef} {...provided.droppableProps} >
             {
               filterSearch()
-              .filter(e => e.marked === marked)
+              .filter(e => e !== undefined && e.marked === marked)
               .map((element, index) => <Element element={element} key={element.id} index={index} />)
             }
             {provided.placeholder} 
--- src/components/Search.js
+++ src/components/Search.js
@@ -46,7 +46,6 @@ export const Search = () => {
         onChange={handleChange}
         placeholder="Buscar ..."
         value={search}
-        autoFocus
       />
       {search && <FontAwesomeIcon icon={faTimes} onClick={clearSearch} />}
     </SearchStyled>
